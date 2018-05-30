9450a65a8af30bac2238e4f09f55a90baddc98b7
Quoc Nguyen <quocnb.vnu@gmail.com>
2018-05-30 17:23:44 +0900

    Initial Commit

---
+++ LeKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeKeyboard
+//
+//  Created by Quoc Nguyen on 2018/05/30.
+//  Copyright © 2018 Quoc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
